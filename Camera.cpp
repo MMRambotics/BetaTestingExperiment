8b00fcfce6e3b2421ee86b445f163b513d091391
Andrew Horsman <minirobotics@gmail.com>
2010-11-05 20:01:54 -0400

    Started EasyCamera class for easy camera feed.

---
+++ Camera.cpp
@@ -0,0 +1,15 @@
+/*
+ * @author:
+ * 	@lead: Andrew Horsman
+ * 	@awesome: Mike Noseworthy
+ * @description: Code that controls the camera feed.
+ */
+
+#ifndef WPILIB_DEFINE
+#define WPILIB_DEFINE
+#include "WPILib.h"
+#endif
+
+class EasyCamera {
+	
+};
