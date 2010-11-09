ece34308647843905795aa8efccf7080db451b7e
Andrew Horsman <minirobotics@gmail.com>
2010-11-08 19:02:48 -0500

    Added potentiometer class.

---
+++ Potentiometer.cpp
@@ -0,0 +1,32 @@
+/*
+ * @author:
+ * 	@lead: Andrew Horsman
+ * 	@other: Mike Noseworthy
+ * @description: Grabs a value from a potentiometer.
+ */
+
+#ifndef WPILIB_DEFINE
+#define WPILIB_DEFINE
+#include "WPILib.h"
+#endif
+
+class Potentiometer {
+	
+	AnalogChannel pot;
+	
+	public:
+	
+		Potentiometer(int portNumber):
+			pot(portNumber)
+		{
+		}
+
+		bool GreaterThanThreshold(float threshold) {
+			return (pot.GetVoltage() > threshold);
+		}
+		
+		bool LessThanThreshold(float threshold) {
+			return (pot.GetVoltage() < threshold);
+		}
+		
+};
