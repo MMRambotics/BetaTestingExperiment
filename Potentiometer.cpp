46118d2912161f0c672401e6581502e4dc652297
Andrew Horsman <minirobotics@gmail.com>
2010-11-10 18:04:30 -0500

    Added a function to Potentiometer that gets the current pot position as a percentage.

--- Potentiometer.cpp
+++ Potentiometer.cpp
@@ -13,12 +13,20 @@
 class Potentiometer {
 	
 	AnalogChannel pot;
+	float         range,
+	              lowerBound;
 	
 	public:
 	
-		Potentiometer(int portNumber):
+		Potentiometer(int portNumber, float lowerBoundVoltage = -1.0, float upperBoundVoltage = -1.0):
 			pot(portNumber)
 		{
+			if (lowerBoundVoltage > upperBoundVoltage)
+				range = lowerBoundVoltage - upperBoundVoltage;
+			else if (upperBoundVoltage > lowerBoundVoltage)
+				range = upperBoundVoltage - lowerBoundVoltage;
+			
+			lowerBound = lowerBoundVoltage;
 		}
 
 		bool GreaterThanThreshold(float threshold) {
@@ -33,4 +41,12 @@ class Potentiometer {
 			return pot.GetVoltage();
 		}
 		
+		float CalculatePosition() {
+			float current = GetRawVoltage();
+			float differenceFromLower = current - lowerBound;
+			
+			if (differenceFromLower == 0.0) return 0;
+			float positionPercentile = 100 / (range / differenceFromLower);
+			return positionPercentile;
+		}
 };
